568ec55e29e665bfbbbda51d5d5d4c729ff19a82
Yossi O <yossi@robotican.co>
2019-12-10 20:38:06 +0200

    Initial commit

---
+++ launch/main.launch.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+from launch import LaunchDescription
+
+# Use this to fix delays on RCLCPP_ log functions:
+os.environ['RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED'] = "1"
+
+
+def generate_launch_description():
+    qos_profile = LaunchConfiguration('qos_profile', default=0)
+    # ns = testing
+    namespace = LaunchConfiguration('ns', default="")
+    return LaunchDescription([
+        Node(
+            node_namespace=namespace,
+            package='ros2-yaml-unnamed-node',
+            node_executable='main',
+            node_name='node_with_params',
+            parameters=[os.path.join(
+                get_package_share_directory('ros2-yaml-unnamed-node'),
+                'param', 'test.param.yaml')],
+            output='screen'),
+        Node(
+            node_namespace=namespace,
+            package='ros2-yaml-unnamed-node',
+            node_executable='main',
+            node_name='another_node_with_same_params',
+            parameters=[os.path.join(
+                get_package_share_directory('ros2-yaml-unnamed-node'),
+                'param', 'test.param.yaml')],
+            output='screen'),
+        Node(
+            node_namespace=namespace,
+            package='ros2-yaml-unnamed-node',
+            node_executable='main',
+            node_name='node_without_params',
+            parameters=[],
+            output='screen'),
+    ])
---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+//
+// Created by yossi on 12/10/19.
+//
+
+#include <rclcpp/rclcpp.hpp>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+class MainNode : public rclcpp::Node
+{
+public:
+    MainNode() : rclcpp::Node("node", rclcpp::NodeOptions())
+    {
+        declare_parameter("param_int", 0);
+        declare_parameter("param_string", "default_text");
+        
+        RCLCPP_INFO(get_logger(), "Integer parameter: %d", get_parameter("param_int").as_int());
+        RCLCPP_INFO(get_logger(), "String  parameter: %s", get_parameter("param_string").as_string().c_str());
+    }
+};
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    
+    rclcpp::spin(std::make_shared<MainNode>());
+    rclcpp::shutdown();
+    return 0;
+}
